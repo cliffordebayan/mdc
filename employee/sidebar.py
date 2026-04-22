7511fb19ad1dff4d5bc3ad4b8be3f574dfbc9714
cliffordebayan <cliffordebayan@gmail.com>
2026-04-22 20:03:43 +0800

    fix sidebar employee

--- employee/sidebar.py
+++ employee/sidebar.py
@@ -7,6 +7,7 @@ To set Horilla sidebar for employee
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as trans
 
+from accessibility.methods import check_is_accessible
 from base.templatetags.basefilters import is_reportingmanager
 
 MENU = trans("Employee")
@@ -93,6 +94,10 @@ def employee_accessibility(request, submenu, user_perms, *args, **kwargs):
     """
     Employee accessibility method
     """
-    return is_reportingmanager(request.user) or request.user.has_perm(
-        "employee.view_employee"
+    cache_key = request.session.session_key + "accessibility_filter"
+    employee = getattr(request.user, "employee_get", None)
+    return (
+        is_reportingmanager(request.user)
+        or request.user.has_perm("employee.view_employee")
+        or check_is_accessible("employee_view", cache_key, employee)
     )
